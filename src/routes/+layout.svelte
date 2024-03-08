bb16a6ef0f9adcf81ed8dd60527ebafc01d9e94c
Jan Bessler <mister.jaybee18@gmail.com>
2024-03-08 22:05:35 +0100

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import './global.css';
+</script>
+
+<div class="main">
+    <div class="header">header</div>
+    <div class="content">
+        <slot></slot>
+    </div>
+    <div class="footer">footer</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<div>this is main, i think?</div>
